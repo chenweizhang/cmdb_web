5597de3f12fa3526dd261ffedf87d0eb3be6b172
cwz <154220026@qq.com>
2019-08-13 18:01:09 +0800

    增加搜索

--- cmdb/urls.py
+++ cmdb/urls.py
@@ -6,10 +6,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from cmdb.views import server_info, del_server_info
+from cmdb.views import server_info, del_server_info, query_server_info
 
 urlpatterns = [
     path('server_info/',server_info,name='server_info'),
     path('del_server_info/',del_server_info,name='del_server_info'),
-
+    path('query_server_info/',query_server_info,name='query_server_info'),
 ]
--- cmdb/views.py
+++ cmdb/views.py
@@ -36,6 +36,18 @@ def acc_logout(request):
 def index(request):
     return render(request, 'index.html')
 
+def pages(obj,page_value,value):
+
+    paginator = Paginator(obj, value)  # 实例化结果集,每页八条数据
+
+    try:
+        # 根据page值返回相应结果
+        reuslt_obj = paginator.page(page_value)
+    except PageNotAnInteger:  # 不存在的页码，返回第一页
+        reuslt_obj = paginator.page(1)
+    except EmptyPage:
+        reuslt_obj = paginator.page(paginator.num_pages)
+    return reuslt_obj
 def server_info(request):
     '''
     资产主机详细信息
@@ -44,18 +56,35 @@ def server_info(request):
     '''
     if request.method == "GET":
         server_info_list = models.hostinfo.objects.all()
-        paginator = Paginator(server_info_list,8)       # 实例化结果集,每页八条数据
+
+        # paginator = Paginator(server_info_list,8)       # 实例化结果集,每页八条数据
         page = request.GET.get("page")      # 接收网页中page值
-        try:
-            # 根据page值返回相应结果
-            server_info_obj = paginator.page(page)
-        except PageNotAnInteger:    # 不存在的页码，返回第一页
-            server_info_obj = paginator.page(1)
-        except EmptyPage:
-            server_info_obj = paginator.page(paginator.num_pages)
+        server_info_obj = pages(server_info_list,page,8)
+        # try:
+        #     # 根据page值返回相应结果
+        #     server_info_obj = paginator.page(page)
+        # except PageNotAnInteger:    # 不存在的页码，返回第一页
+        #     server_info_obj = paginator.page(1)
+        # except EmptyPage:
+        #     server_info_obj = paginator.page(paginator.num_pages)
         return render(request,"member-list.html",{"server_info_list":server_info_obj})
+def query_server_info(request):
 
+    if request.method == "GET":
+        error_msg = ''
+        hostname = request.GET.get("hostname")
+        print('111111[%s]' % hostname)
+        page = request.GET.get("page")
 
+        if hostname is not None:
+            server_info_list = hostinfo.objects.filter(hostname__icontains=hostname)
+            server_info_obj = pages(server_info_list,page,8)
+        else:
+            error_msg = '请输入关键字'
+            print(error_msg)
+        # return HttpResponse('ok')
+        return render(request,"member-list.html",{"server_info_list":server_info_obj,
+                                            "error_msg":error_msg})
 def del_server_info(request):
 
     if request.method == "POST":
