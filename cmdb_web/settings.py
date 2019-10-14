e8a8b31f02dcd08b23bfc4e7747deb535028c425
cwz <154220026@qq.com>
2019-10-14 17:56:01 +0800

    监控anget

--- cmdb_web/settings.py
+++ cmdb_web/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'cmdb_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cmdb',
+        'USER': 'cmdb',
+        'PASSWORD': 'cmdb@123',
+        'HOST': 'rm-bp197625v8pjnwl15lo.mysql.rds.aliyuncs.com',
     }
 }
 
